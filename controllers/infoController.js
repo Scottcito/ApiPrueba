c0d2a380ceb755de2d05b1b72f0184e717f880c9
Natis1423 <145591328+Natis1423@users.noreply.github.com>
2024-07-14 15:43:20 -0500

    Update infoController.js

--- controllers/infoController.js
+++ controllers/infoController.js
@@ -1,27 +1,23 @@
-// app.js (o donde tengas configurado tu servidor)
-import express from 'express';
 import mysql from 'mysql2/promise';
-import db from './config/database.js';
+import db from '../config/database.js';
 
-const app = express();
+export default class InfoController {
+  static async index(req, res) {
+    const { texto } = req.query; // Obtén el texto de los parámetros de consulta
+    console.log(`Texto recibido en la consulta: ${texto}`); // Imprime el texto en la consola del servidor
 
-app.get('/api/consultarTexto', async (req, res) => {
-  const { texto } = req.query;
-  let connection;
-
-  try {
-    connection = await mysql.createConnection(db);
-    const [result] = await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra = ?', [texto]);
-    console.log(result);
-    res.json({ data: result });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  } finally {
-    if (connection) {
-      await connection.end();
+    let connection;
+    try {
+      connection = await mysql.createConnection(db);
+      const [result] = await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra = ?', [texto]);
+      console.log(result); // Imprime el resultado de la consulta en la consola del servidor
+      res.json({ data: result });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    } finally {
+      if (connection) {
+        await connection.end();
+      }
     }
   }
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
