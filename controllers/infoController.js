5e477ca06ce5ee2a70a383e757427eec48a58285
Natis1423 <145591328+Natis1423@users.noreply.github.com>
2024-07-14 15:58:43 -0500

    Update infoController.js

--- controllers/infoController.js
+++ controllers/infoController.js
@@ -1,23 +1,21 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2/promise'
 import db from '../config/database.js';
-
-export default class InfoController {
-  static async index(req, res) {
-    const { texto } = req.query; // Obtén el texto de los parámetros de consulta
-    console.log(`Texto recibido en la consulta: ${texto}`); // Imprime el texto en la consola del servidor
-
-    let connection;
-    try {
-      connection = await mysql.createConnection(db);
-      const [result] = await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra = ?', [texto]);
-      console.log(result); // Imprime el resultado de la consulta en la consola del servidor
-      res.json({ data: result });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    } finally {
-      if (connection) {
-        await connection.end();
-      }
+export default class infoController{
+    static async index(req,res){
+        let connection;
+        try{
+            connection=await mysql.createConnection(db)
+            const [result]=await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra = "a"')
+            console.log(result)
+            res.json(result)
+        }
+        catch(error){
+            res.status(500).json({'error :':error.message})
+        }
+        finally{
+            if(connection){
+                await connection.end()
+            }
+        }
     }
-  }
 }
