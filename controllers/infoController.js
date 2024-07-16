b3748210418cbe5bd3938da078617492b92dfa12
Natis1423 <145591328+Natis1423@users.noreply.github.com>
2024-07-16 12:18:44 -0500

    Update infoController.js

--- controllers/infoController.js
+++ controllers/infoController.js
@@ -2,10 +2,13 @@ import mysql from 'mysql2/promise'
 import db from '../config/database.js';
 export default class infoController{
     static async index(req,res){
+        let texto=texto
+        console.log(texto)
         let connection;
         try{
             connection=await mysql.createConnection(db)
-            const [result]=await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra = "a"')
+            const [result]=await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra ="a"')
+            console.log(texto)
             console.log(result)
             res.json(result)
         }
