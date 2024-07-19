7c7e84bb40206afac0f9627d71a6649de2ce6378
Natis1423 <145591328+Natis1423@users.noreply.github.com>
2024-07-18 21:01:16 -0500

    Update infoController.js

--- controllers/infoController.js
+++ controllers/infoController.js
@@ -4,13 +4,13 @@ export default class infoController{
     static async index(req,res){
         let connection;
         try{
-            const textoo= req.texto
-            console.log(textoo)
+             const { texto } = req.body;
+            console.log(texto)
             connection=await mysql.createConnection(db)
-            const [result]=await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra ="a"')
+            const [result]=await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra = ?',[texto])
             console.log(result)
             res.json(result)
-            res.json(textoo)
+            res.json(texto)
         }
         catch(error){
             res.status(500).json({'error :':error.message})
