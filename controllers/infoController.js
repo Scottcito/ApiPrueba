562bd27d35306e1ea1ca8b36f248da15ef46a1d3
Natis1423 <145591328+Natis1423@users.noreply.github.com>
2024-07-22 11:54:02 -0500

    Update infoController.js

--- controllers/infoController.js
+++ controllers/infoController.js
@@ -6,7 +6,7 @@ export default class infoController{
         try{
             const texto= req.body.texto;
             connection=await mysql.createConnection(db)
-            const [result]=await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra = ?',[texto])
+            const [result]=await connection.execute('SELECT imagen_palabra FROM Palabras WHERE palabra = "a"')
             console.log(result)
             res.json(result)
             console.log(texto)
